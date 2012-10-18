f1e4bce6163d949d6216ad93f1620742bc2c52e2
rrezyk <rrezyk@gmail.com>
2012-10-18 17:20:32 +0800

    remove some old app

--- project/songs/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('project.songs.views',
-    (r'^(?P<song_slug>[-\w]+)/$', 'show_song', 
-       {'template_name': 'songs/song.html'}, 'songs_song'),
-
-)
